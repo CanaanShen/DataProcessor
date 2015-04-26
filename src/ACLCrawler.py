9795c52504191bc8f5bdb2c54cd7666128fad1fd
Yueshen Xu <xyshzjucs@zju.edu.cn>
2015-04-26 17:08:20 -0500

    acl

--- src/ACLCrawler.py
+++ src/ACLCrawler.py
@@ -5,22 +5,79 @@ Created on Apr 26, 2015
 '''
 import os
 import urllib.request
+import re
 from bs4 import BeautifulSoup
 
 class ACLCrawler:
     def crawlACL(self, url, outputDir, year):
         opener = urllib.request.urlopen(url)
         content = opener.read()
+        content = content.decode('utf-8').encode('cp850', 'replace').decode('cp850')
         soup = BeautifulSoup(content)
-        latexmlLinks = soup.find_all('a', text="")
+        latexmlLinks = soup.find_all('a', text="latexml")
+        num = 0
+        
+        for latexmlLink in latexmlLinks:
+            link = latexmlLink.get('href')
+            fullLink = url + link
+            
+            opener = urllib.request.urlopen(fullLink)
+            content = opener.read()
+            content = content.decode('utf-8').encode('cp850', 'replace').decode('cp850')
+            soup = BeautifulSoup(content)
+            abstract = soup.find('p', class_="ltx_p").text
+            
+            file = os.path.join(outputDir, year + str(num)+".txt")
+            fileHandler = open(file, "w")
+            fileHandler.write(abstract)
+            fileHandler.close()
+            num = num + 1
+        #for
+
+    #def
+    def crawlACL13(self, url, outputDir, year, prefix):
+        opener = urllib.request.urlopen(url)
+        content = opener.read()
+        content = content.decode('utf-8').encode('cp850', 'replace').decode('cp850')
+        soup = BeautifulSoup(content)
+        paperLinks = soup.find_all('a', href=re.compile("accepted/\d+.html"))
+        num = 0
+        
+        for paperLink in paperLinks:
+            link = paperLink.get('href')
+            fullLink = prefix + link
+            
+            opener = urllib.request.urlopen(fullLink)
+            content = opener.read()
+            content = content.decode('utf-8').encode('cp850', 'replace').decode('cp850')
+            soup = BeautifulSoup(content)
+            div = soup.find_all("div")
+            
+            abstract = ""
+            for content in div[0].p.br.p.p.contents:
+                content = str(content)
+
+                if "<" in content and ">" in content:
+                    print(content)
+                else:
+                    abstract = abstract + content
+            #for
+            
+            file = os.path.join(outputDir, year + str(num)+".txt")
+            fileHandler = open(file, "w")
+            fileHandler.write(abstract)
+            fileHandler.close()
+            num = num + 1
+        #for
 
     #def
 #class
 
-url = "https://www.aclweb.org/anthology/P/P14/"
+url = "http://acl2013.org/site/accepted-papers.html"
 rootDir = "..\\acl"
-year = "14"
+year = "13"
 outputDir = os.path.join(rootDir, year)
+prefix = "http://acl2013.org/site/"
 aclCrawler = ACLCrawler()
-aclCrawler.crawlACL(url, outputDir, year)
+aclCrawler.crawlACL13(url, outputDir, year, prefix)
 print("Program ends")
